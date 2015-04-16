762af5f07615f0509e5c932cd8ee83e35ae4436e
yhe10 <yhe10@masonlive.gmu.edu>
2015-04-16 14:17:36 -0400

    second commit

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
+        Person person = new Person("Dorian");
         assertEquals("Larry", person.getName());
     }
 }
